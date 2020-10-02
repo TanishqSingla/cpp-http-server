79108ff5c49eb759647707ea398d8ccb8b0874c1
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-02 12:35:10 +0530

    Added Socket for listening

--- cpp-http-server/main.cpp
+++ cpp-http-server/main.cpp
@@ -4,6 +4,8 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
+#define DEFAULT_PORT "27015"
+
 int main() {
 	WSADATA wsaData;
 	int iResult;
@@ -14,5 +16,35 @@ int main() {
 		printf("WSAStartup failed: %d\n", iResult);
 		return 1;
 	}
+
+	struct addrinfo* result = NULL, * ptr = NULL, hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP; 
+	hints.ai_flags = AI_PASSIVE;
+
+	//Resolve the local address and port to be used by the server
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed %d\n", iResult); 
+		WSACleanup();
+		return 1;
+	}
+
+	//Initializing socket for listening connection from client
+	SOCKET ListenSocket = INVALID_SOCKET;
+
+	//Creating Socket for the server to listen for client connection
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	
+	//Checking for errors
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		return 1;
+	}
+
 	return 0;
 }
