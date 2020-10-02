c8253cac6dd43c73b00adcf7f34f67aa4f2b5bec
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-02 11:52:18 +0530

    Initializing winsock

--- cpp-http-server/main.cpp
+++ cpp-http-server/main.cpp
@@ -5,5 +5,14 @@
 #pragma comment(lib, "Ws2_32.lib")
 
 int main() {
+	WSADATA wsaData;
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
 	return 0;
 }
